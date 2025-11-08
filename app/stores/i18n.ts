3a83cb69811d2ff3e3104ae4783da00fffd0028d
Manoah Tervoort <149895ja@gmail.com>
2025-11-08 21:45:38 +0100

    feat: fetch keys

--- app/stores/i18n.ts
+++ app/stores/i18n.ts
@@ -1,5 +1,6 @@
 type State = {
-  translations: Record<AppLocale, { key: string; value: string }[]>
+  keys: Set<string>
+  translations: Record<AppLocale, { id: number; key: string; value: string }[]>
 }
 
 export const useI18nStore = defineStore('i18n', {
@@ -13,17 +14,39 @@ export const useI18nStore = defineStore('i18n', {
         error,
         status,
         statusText
-      } = await supabase.from('translations').select('key, value').eq('lang', locale)
+      } = await supabase.from('translations').select('id, key, value').eq('lang', locale)
 
       if (error) {
         supabaseService.handleError(error, status, statusText)
       } else if (translations) {
         this.translations[locale] = translations
       }
+    },
+    async fetchKeys(refresh = false) {
+      if (this.keys.size > 0 && !refresh) return
+
+      const supabase = useSupabaseClient()
+
+      const [names, titles] = await Promise.all([
+        supabase.from('people').select('name'),
+        supabase.from('events').select('title')
+      ])
+
+      if (names.error) {
+        supabaseService.handleError(names.error, names.status, names.statusText)
+      } else if (titles.error) {
+        supabaseService.handleError(titles.error, titles.status, titles.statusText)
+      } else {
+        this.keys = new Set([
+          ...(names.data?.map((n) => n.name) ?? []),
+          ...(titles.data?.map((t) => t.title) ?? [])
+        ])
+      }
     }
   },
   persist: true,
   state: (): State => ({
+    keys: new Set<string>(),
     translations: {
       en: [],
       nl: []
