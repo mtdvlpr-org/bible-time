48727b40cc10075ddf7f4441d568f3b16e4f971c
Manoah Tervoort <46671786+mtdvlpr@users.noreply.github.com>
2025-12-23 08:34:36 +0100

    fix: convert docid to pubId

--- server/utils/jw/general.ts
+++ server/utils/jw/general.ts
@@ -54,7 +54,7 @@ export const extractPubId = (input: string): null | PubId => {
     if (lank && isPubId(lank)) return lank
 
     const docid = url.searchParams.get('docid')
-    if (docid && isPubId(docid)) return docid
+    if (docid) return 'docid-' + docid as PubId
 
     // eslint-disable-next-line no-useless-escape
     const idRegex = /\/((?:pub|docid)-[^\/]+)/
